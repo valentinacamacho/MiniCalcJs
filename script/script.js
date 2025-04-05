67e602a35e6fc7050573255f642f150ae92bd2bd
valentinacamacho <valentinacamacho0206@gmail.com>
2025-04-04 19:13:55 -0500

    They were validated if the data entered is numeric with isNaN

--- script/script.js
+++ script/script.js
@@ -2,4 +2,12 @@ function calcular(operacion){
     const input1 = parseFloat(document.getElementById("number1").value);
     const input2 = parseFloat(document.getElementById("number2").value);
     const resul = document.getElementById("result");
+
+    if(isNaN(input1) || isNaN(input2)){
+        resul.textContent = "Ingresa ambos números en las cajas de texto";
+        resul.style.color = "red";
+        return;
+    }
+
+
 }
