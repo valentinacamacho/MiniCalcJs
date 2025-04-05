21bb2d41df24169c8ce3b75a4259b886d18c3622
valentinacamacho <valentinacamacho0206@gmail.com>
2025-04-04 19:00:32 -0500

    In the js file, declare variables within the calculate function by calling an operation parameter that will contain the text box variables that will access the dom elements.

---
+++ script/script.js
@@ -0,0 +1,5 @@
+function calcular(operacion){
+    const input1 = parseFloat(document.getElementById("number1").value);
+    const input2 = parseFloat(document.getElementById("number2").value);
+    const resul = document.getElementById("result");
+}
